e75070546f9cd87ef6869a1b52f6014074d3cd2e
indranil-innoraft <indranil.roy@innoraft.com>
2023-07-03 19:19:26 +0530

    FT2023-306: Custom access checking for current user.

--- root/web/modules/custom/routing/src/Controller/RoutingController.php
+++ root/web/modules/custom/routing/src/Controller/RoutingController.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\routing\Controller;
 
+use Drupal\Core\Access\AccessResult;
+use Drupal\user\Entity\User;
 use Drupal\Core\Controller\ControllerBase;
 
 /**
@@ -21,5 +23,20 @@ class RoutingController extends ControllerBase {
       '#type' => 'markup',
       '#markup' => t('Welcome to my drupal website.'),
     ];
+  }
+
+  /**
+   * Custom access checking for current user.
+   *
+   * @return object
+   */
+  public function accessCheck() {
+    $user = User::load(\Drupal::currentUser()->id());
+    if ($user->hasPermission('access the custom page')) {
+      return AccessResult::allowed();
+    }
+    else {
+      return AccessResult::forbidden();
+    }
   }
 }
